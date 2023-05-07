a2d435a9a82f1f65b1819bcc677aa90b90521d5c
rk2040 <rk2040@gmail.com>
2023-05-07 20:50:11 -0300

    css y checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,7 +3,8 @@ import { useState, useContext, useEffect } from 'react'
 import { CarritoContext } from '../../context/CarritoContext'
 import { db } from '../../services/firebase/config'
 import { Timestamp, addDoc, doc, collection, documentId, getDocs, query, updateDoc, where, writeBatch } from 'firebase/firestore'
-
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom'
 
 
 const Checkout = () => {
@@ -17,14 +18,20 @@ const Checkout = () => {
     const [ordenId, setOrdenId] = useState('');
     const [error, setError] = useState('');
 
+    const btnComprar = <button className='btnComprar' type='submit'> Finalizar Compra </button>
+    const btnSeguirViendo = <Link className='linkBtnCarrito' to='/tienda'> Seguir Viendo </Link>
+    const [verificado, setVerificado] = useState(true);
+    
     const handleSubmit = (event)=> {
         event.preventDefault();
+
 
         //Validamos que los campos esten completos
         if(!nombre || !apellido || !telefono || !email || !emailConfirmacion){
             setError("Complete todos los campos.");
             return;
         }
+
         //Validamos que el email coincida
         if(email !== emailConfirmacion){
             setError("El email no coincide.")
@@ -48,26 +55,44 @@ const Checkout = () => {
             fecha: Timestamp.fromDate(new Date()),
         };
 
+        if(orden.items < 1){
+            setError("Su carrito esta vacio.")
+            console.log("Su carrito esta vacio.")
+            return;
+        }
+
         //Guardamos la orden en la BBDD
         //const confirmarOrden = ()=> {   // Agregue funcion confirmarOrden para usarla con validadores de cantidad de stock y demas
             addDoc(collection(db, "ordenes"), orden)
-            .then( (docRef)=> {
+                .then( (docRef)=> {
+                    setOrdenId(docRef.id);
+                    emptyCart();
+
+                Swal.fire({
+                    icon: 'success',
+                    title: `${nombre} gracias por tu compra. Tu número de orden es: ${docRef.id}`
+                    })
+                setVerificado(false);
+                })
+                .catch( (error)=> {
+                    console.log("Error al crear la orden.", error);
+                    setError("Se produjo un error al generar la orden.");
+
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Se produjo un error al generar la orden.',
+                        })
+                })
                 
-                setOrdenId(docRef.id);
-                emptyCart();
-            })
-            .catch( (error)=> {
-                console.log("Error al crear la orden.", error);
-                setError("Se produjo un error al generar la orde.");
-            })
-            
+                
             setNombre("");
             setApellido("");
             setTelefono("");
             setTelefono("");
             setEmail("");
             setEmailConfirmacion("");
-        //}
+
+            setError("");
 
     }
     
@@ -79,7 +104,7 @@ const Checkout = () => {
 
     return (
         <div>
-            <h2> Checkout </h2>
+            <h2 className='checkoutFomr'> Checkout </h2>
             <form onSubmit={ handleSubmit }>
                 {carrito.map( (producto)=> {
                     <div key={producto.item.id}>
@@ -92,27 +117,28 @@ const Checkout = () => {
                 })}
                 <hr/>
                 <div className='dataForm'>
-                    <label htmlFor="">Nombre</label>
-                    <input type="text" value={nombre} onChange={(e)=> setNombre(e.target.value)} />
+                    <label className='labelForm' htmlFor="">Nombre</label>
+                    <input className='inputForm' type="text" value={nombre} onChange={(e)=> setNombre(e.target.value)} placeholder='Tu nombre'/>
 
-                    <label htmlFor="">Apellido</label>
-                    <input type="text" value={apellido} onChange={(e)=> setApellido(e.target.value)} />
+                    <label className='labelForm' htmlFor="">Apellido</label>
+                    <input className='inputForm' type="text" value={apellido} onChange={(e)=> setApellido(e.target.value)} placeholder='Tu apellido'/>
 
-                    <label htmlFor="">Telefono</label>
-                    <input type="number" value={telefono} onChange={(e)=> setTelefono(e.target.value)} />
+                    <label className='labelForm' htmlFor="">Telefono</label>
+                    <input className='inputForm' type="number" value={telefono} onChange={(e)=> setTelefono(e.target.value)} placeholder='41234567'/>
 
-                    <label htmlFor="">Email</label>
-                    <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+                    <label className='labelForm' htmlFor="">Email</label>
+                    <input className='inputForm' type="email" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder='tuemail@email.com'/>
 
-                    <label htmlFor="">Confirmar Email</label>
-                    <input type="email" value={emailConfirmacion} onChange={(e)=> setEmailConfirmacion(e.target.value)} />
+                    <label className='labelForm' htmlFor="">Confirmar Email</label>
+                    <input className='inputForm' type="email" value={emailConfirmacion} onChange={(e)=> setEmailConfirmacion(e.target.value)} placeholder='tuemail@email.com'/>
                 </div>
                 {error && <p style={{color:'red'}} > {error} </p>}
-                <button type='submit'> Finalizar Compra </button>
+
+                { verificado ? btnComprar  : btnSeguirViendo }
             </form>
             {
                 ordenId && (
-                    <strong>Tu compra se genero con exito. <br/> Tu numero de orden es: {ordenId} </strong>
+                    <p className='numeroOrden'>Guarda tu número de orden para darle seguimiento. <br/> Tu número de orden es: <strong>{ordenId}</strong> </p>
                 )
             }
 
--- src/components/PaginaEnConstruccion/PaginaEnConstruccion.jsx
+++ src/components/PaginaEnConstruccion/PaginaEnConstruccion.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './PaginaEnConstruccion.css'
+import { Link } from 'react-router-dom';
 
 const PaginaEnConstruccion = () => {
     const imgConstruccion = "https://static.vecteezy.com/system/resources/previews/000/192/096/original/vector-website-under-construction.png";
@@ -7,6 +8,7 @@ const PaginaEnConstruccion = () => {
         <div className='contenedorConstruccion'>
             <h2 className='sitioEnContruccion'>Sitio en Construcción</h2> 
             <img className='imgEnConstruccion' src={imgConstruccion} alt="Sitio en Construccion" />
+            <Link className='linkBtnCarrito' to='/tienda'> Seguir Viendo </Link>
         </div>
     )
 }
