647bae2d78c44a3218d682d6a229164aaa99d93d
rk2040 <rk2040@gmail.com>
2023-05-10 20:13:21 -0300

    Descuento stock

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,8 @@ import Cart from './components/Cart/Cart';
 import Checkout from './components/Checkout/Checkout';
 import PaginaHome from './components/PaginaHome/PaginaHome';
 import PaginaEnConstruccion from './components/PaginaEnConstruccion/PaginaEnConstruccion';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import { CarritoProvider } from './context/CarritoContext';
-
-import Productos from './components/Productos/Productos';
-
-
 
 function App() {
   return (
@@ -24,14 +18,11 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path='/' element={ <PaginaHome/> }/>
-          
-          {/* <Route path='/' element={ <Productos/> }/> */}
-          
           <Route path='/tienda' element={ <ItemListContainer/> }/>
           <Route path='/categoria/:idCategoria' element={ <ItemListContainer/> }/>
           <Route path='/item/:idItem' element={ <ItemDetailContainer/> }/>
           <Route path='/cart' element={<Cart/>}/>
-          <Route path='/checkout' element={<Checkout/>}/>  {/* cambiar por path=/'Checkout' */}
+          <Route path='/checkout' element={<Checkout/>}/>
           <Route path='/*' element={ <PaginaEnConstruccion/> }/> 
         </Routes>
       </CarritoProvider>  
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,11 +1,10 @@
 import './Checkout.css'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { CarritoContext } from '../../context/CarritoContext'
 import { db } from '../../services/firebase/config'
-import { Timestamp, addDoc, doc, collection, documentId, getDocs, query, updateDoc, where, writeBatch } from 'firebase/firestore'
+import { Timestamp, addDoc, doc, collection, query, updateDoc, onSnapshot } from 'firebase/firestore'
 import Swal from 'sweetalert2';
 import { Link } from 'react-router-dom'
-
 
 const Checkout = () => {
     const {carrito, emptyCart} = useContext(CarritoContext);
@@ -21,10 +20,36 @@ const Checkout = () => {
     const btnComprar = <button className='btnComprar' type='submit'> Finalizar Compra </button>
     const btnSeguirViendo = <Link className='linkBtnCarrito' to='/tienda'> Seguir Viendo </Link>
     const [verificado, setVerificado] = useState(true);
-    
+
+    // Bajar stock ================================
+    const [stockProductos, setStockProductos] = useState([]); 
+    useEffect( ()=> {
+        const q = query(collection(db, "productos"));
+
+        const modificaStock = onSnapshot(q, function(querySnapShot) {
+            const docs = [];
+            querySnapShot.forEach(function (doc) {
+                docs.push({ id: doc.id, ...doc.data() });
+            });
+            setStockProductos(docs);
+        });
+        return ()=> { modificaStock() }
+    }, []);
+
+    const descuentaStock = (id, cantidad)=> {
+        const productoref = doc(db, "productos", id)
+        const productoAux = stockProductos.find(prod => prod.id === id)
+
+        if(productoAux){
+            updateDoc(productoref, { stock: productoAux.stock - cantidad })
+                .then( ()=> console.log(`Se compro: ${productoref}`))
+                .catch( (error)=> console.log(error))
+        }
+    }
+    // Bajar stock ================================
+
     const handleSubmit = (event)=> {
         event.preventDefault();
-
 
         //Validamos que los campos esten completos
         if(!nombre || !apellido || !telefono || !email || !emailConfirmacion){
@@ -55,52 +80,46 @@ const Checkout = () => {
             fecha: Timestamp.fromDate(new Date()),
         };
 
-        if(orden.items < 1){
+        orden.items.map(prod => (descuentaStock(prod.id, prod.cantidad)));
+
+        if(orden.items < 1){    // Compruebo si hay hay productos en en pepido para generar la orden
             setError("Su carrito esta vacio.")
             console.log("Su carrito esta vacio.")
             return;
         }
-
         //Guardamos la orden en la BBDD
         //const confirmarOrden = ()=> {   // Agregue funcion confirmarOrden para usarla con validadores de cantidad de stock y demas
-            addDoc(collection(db, "ordenes"), orden)
-                .then( (docRef)=> {
-                    setOrdenId(docRef.id);
-                    emptyCart();
+        addDoc(collection(db, "ordenes"), orden)
+            .then( (docRef)=> {
+                setOrdenId(docRef.id);
 
                 Swal.fire({
                     icon: 'success',
                     title: `${nombre} gracias por tu compra. Tu número de orden es: ${docRef.id}`
                     })
+
                 setVerificado(false);
-                })
-                .catch( (error)=> {
-                    console.log("Error al crear la orden.", error);
-                    setError("Se produjo un error al generar la orden.");
+                emptyCart();
+            })
+            .catch( (error)=> {
+                console.log("Error al crear la orden.", error);
+                setError("Se produjo un error al generar la orden.");
 
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Se produjo un error al generar la orden.',
-                        })
-                })
-                
-                
-            setNombre("");
-            setApellido("");
-            setTelefono("");
-            setTelefono("");
-            setEmail("");
-            setEmailConfirmacion("");
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Se produjo un error al generar la orden.',
+                    })
+            })
+            
+        setNombre("");
+        setApellido("");
+        setTelefono("");
+        setTelefono("");
+        setEmail("");
+        setEmailConfirmacion("");
 
-            setError("");
-
+        setError("");
     }
-    
-    //==========================================================================================
-    // Vemos como completamos esto o si usamos funciones sueltas para hacer validaciones de stock y para descontar stock de la BBDD
-
- //==========================================================================================
-
 
     return (
         <div>
@@ -134,15 +153,13 @@ const Checkout = () => {
                 </div>
                 {error && <p style={{color:'red'}} > {error} </p>}
 
-                { verificado ? btnComprar  : btnSeguirViendo }
+                { verificado ? btnComprar : btnSeguirViendo }
             </form>
             {
                 ordenId && (
                     <p className='numeroOrden'>Guarda tu número de orden para darle seguimiento. <br/> Tu número de orden es: <strong>{ordenId}</strong> </p>
                 )
             }
-
-            {/* <Productos/> */}
         </div>
     );
 }
